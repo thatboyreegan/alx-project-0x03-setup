788daad94bdc213ab93a7cba2ba1e94998ed4f65
thatboyreegan <reaganronald491@gmail.com>
2025-01-12 15:12:25 +0300

    fix: moving the interfaces to the right folder

--- alx-project-0x03/pages/index.tsx
+++ alx-project-0x03/pages/index.tsx
@@ -1,9 +1,8 @@
 import Button from "@/components/common/Button";
+import { PageRouteProps } from "@/interface";
 import { useRouter } from "next/router";
 
-interface PageRouteProps {
-  pageRoute: string
-}
+
 
 export default function Home() {
   const router = useRouter()
