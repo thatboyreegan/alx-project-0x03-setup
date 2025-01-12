d6869b86c9a5a3670227a330a70ed9bbd5385307
thatboyreegan <reaganronald491@gmail.com>
2025-01-12 15:45:28 +0300

    fix: Js syntax

--- alx-project-0x03/pages/index.tsx
+++ alx-project-0x03/pages/index.tsx
@@ -4,7 +4,7 @@ import { useRouter } from "next/router";
 
 
 export default function Home() {
-  const router = useRouter()
+  const router = useRouter();
 
   // Imeperative routing with useRouter
   const routeToNextPage = ({ pageRoute }: PageRouteProps) => {
